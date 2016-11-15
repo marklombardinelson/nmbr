88e190d5cfcc488b1b5e4102e6bf48fb643a20be
MarkLombardiNelson <marklombardinelson@gmail.com>
2016-11-15 17:50:25 -0500

    Final Submission + Adventure Mode

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,8 +1,21 @@
+def right_answer right_answer
+  puts "Wow! That's Awesome!"
+end
+
+def wrong_answer_low wrong_answer_low
+  puts "Woah that's low... try again!"
+end
+
+def wrong_answer_high wrong_answer_high
+  puts "Woah that's high... try again!"
+end
+
 # Pick a Random Number
 computer_number = rand(1..100)
 
 # Add count + 1
 count = 0
+guesses = []
 
 loop do
 # Ask User For Number
@@ -12,15 +25,23 @@ guess = gets.chomp.to_i
 count = count + 1
 # If the guess is less than number, say "Too Low"
 if guess < computer_number
-  puts "Too Low"
+  wrong_answer_low "Too Low"
 end
 # If the guess is more than the number, say "Too high"
 if guess > computer_number
-  puts "Too High"
+  wrong_answer_high "Too High"
 end
+
+# If user inputs same number twice
+if guesses.include? (guess)
+  puts "Lol,  you messed up!"
+end
+
+guesses << guess
+
 # If you guess the number, stop asking for number and say "You win"
 if guess == computer_number
-  puts "You Win"
+  right_answer "You Win"
   break
 end
 # If the total count of all guesses is > than 5 times wrong, say "You Lose"
