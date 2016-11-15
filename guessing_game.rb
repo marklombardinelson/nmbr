93cf433e42f847d8b7fe49447eeac076796383ea
MarkLombardiNelson <marklombardinelson@gmail.com>
2016-11-15 14:27:46 -0500

    Ruby Comments

---
+++ guessing_game.rb
@@ -0,0 +1,9 @@
+# Pick a Random Number
+
+# Ask User For Number
+# Add count + 1
+# If the guess is less than number, say "Too Low"
+# If the guess is more than the number, say "Too high"
+# If you guess the number, stop asking for number and say "You win"
+
+# If the total count of all guesses is > than 5 times wrong, say "You Lose"
